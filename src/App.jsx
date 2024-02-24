8c67ac9727330fbdf0feda64029ff4dc344b4766
Thekey213 <azdilinga@gmail.com>
2024-02-24 17:56:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import MyComponent from './components/MyComponent'
+
+function App() {
+ 
+
+  return (
+    
+    <MyComponent/>
+    
+      
+  )
+}
+
+export default App
